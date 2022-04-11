1d03949b1f549d17ddeee80d4e53122cbe5cee63
SHUYUAN66 <49173407+SHUYUAN66@users.noreply.github.com>
2022-04-10 21:48:14 -0400

    try4

--- spark-examples/dj30-feeder.py
+++ spark-examples/dj30-feeder.py
@@ -9,21 +9,7 @@ dir_path = os.path.realpath(__file__)
 dj30_df = pd.read_csv(os.path.dirname(dir_path) + '/data/dj30.csv')
 dj30_df.dropna(inplace=True)
 dj30_df = dj30_df.loc[:,['Long Date', 'Close']]
-dj30 = dj30_df['Close'].tolist()
 
-class CloseFeed:
-    source = None
-    def __init__(self, source):
-        self.source = source
-
-    def getClose(self):
-        i = 0
-        while True:
-            time.sleep(0.25)
-            i += 1
-            yield self.source[i-1]
-
-dj30CloseFeed = CloseFeed(dj30)
-
-for close in dj30CloseFeed.getClose():
-    print (close, flush = True)
+for _, row in dj30_df.iterrows():
+    print(row['Long Date'], row['Close'], flush = True)
+    time.sleep(1.0)
