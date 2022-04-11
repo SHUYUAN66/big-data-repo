4e0a8422318d089ce0f53de987e3e246a13cdbd9
SHUYUAN66 <49173407+SHUYUAN66@users.noreply.github.com>
2022-04-10 21:04:17 -0400

    try1

--- tf-idf/test.py
+++ tf-idf/test.py
@@ -9,6 +9,8 @@ from operator import itemgetter
 import sys
 import numpy as np
 import pprint
+import math
+from numpy import linalg as LA
 pp = pprint.PrettyPrinter(indent = 4, width = 80)
 
 def main(argv):
@@ -39,11 +41,12 @@ def main(argv):
             # count was not a number, so silently
             # ignore/discard this line
             pass
-    
     tfidf = calculateTFIDF(wcss) # Implement this function
+   
 
 def calculateTFIDF(wcss):
     expected = '''
+    
     fnam1
         word1 tfidf1
         word2 tfidf2
@@ -51,14 +54,36 @@ def calculateTFIDF(wcss):
           ::
         wordn tfidfn
     '''
-    
-    for fnam in sorted(wcss):
-        wcs = wcss[fnam]
-        print ('\n\n', fnam)
-        sorted_wcs = dict( sorted(wcs.items(), key = lambda item: item[1], reverse = True))
-        for w in sorted_wcs:
-            print (w, sorted_wcs[w])
-    return None
+    bows = []
+    wordset = set()
+    for i in sorted(wcss):
+        bows+=list(wcss[i].keys())  
+        wordset = wordset.union(set(wcss[i].keys()))
 
+    N=len(wcss.keys())
+    counter = dict(collections.Counter(bows))
+    idf_diz=dict.fromkeys(wordset,0)
+    for w in wordset:
+      idf_diz[w] = np.log((1+N)/(1+counter[w]))+1
+    
+    for fname in sorted(wcss):
+        bow = wcss[fname]
+        print(fname)
+        #calculateTF
+        termfreq_diz = dict.fromkeys(wordset,0)
+        counter1 =  dict(collections.Counter(bow))
+        for w in bow:
+            termfreq_diz[w]=counter1[w]/len(bow)
+        tf_idf_diz = dict.fromkeys(wordset,0)
+        for w in wordset:
+            tf_idf_diz[w]=termfreq_diz[w] * idf_diz[w]
+        tf_idf_diz[w]=termfreq_diz[w]*idf_diz[w]
+        tdidf_values = list(tf_idf_diz.values())
+        l2_norm = LA.norm(tdidf_values) 
+        tf_idf_norm = {w:tf_idf_diz[w]/l2_norm for w in wordset}
+        sorted_tf_idf_norm = dict( sorted(tf_idf_norm.items(), key = lambda item: item[1], reverse = True))
+        for w in sorted_tf_idf_norm:
+            print("\t" + w + " " + str(sorted_tf_idf_norm[w]))
+    return None
 if __name__ == "__main__":
     main(sys.argv)
